08949d1ff9a931c5e46d0343490c33d4064015e0
shravanigolla662 <shravaniramulusravs@gmail.com>
2024-07-19 20:32:19 +0530

    Add files via upload

---
+++ Voting_Right.java
@@ -0,0 +1,18 @@
+package anudip;
+
+public class Voting_Right {
+	public static void main(String args[])
+	{
+	int age=25;   //intilizing and declaring the age and voting age
+	int voting_age=18;
+	
+	// Compare age and votingAge
+	if(age>=voting_age)
+		System.out.println("Old enough to vote!");	//eligible for voting
+	else
+		System.out.println("Not Old enough to vote!");//not eligible for voting
+	
+	}
+	
+
+}
